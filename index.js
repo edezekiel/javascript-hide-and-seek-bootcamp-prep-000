653542cdafbace9a12147b1923c9dbed5bfa9fcd
ed.a.ezekiel <ed.a.ezekiel@gmail.com>
2018-09-17 18:35:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+function getFirstSelector(selector){
 
+const first = document.querySelector(selector)
+// return first element that it matches
+
+return first
+}
+
+function nestedTarget() {
+
+var target = document.getElementById("nested")
+return target
+
+}
+
+function increaseRankBy(n){
+
+  document.querySelectorAll('ul.ranked-list')
+
+
+  //get the ranked list nodes
+  var allLi = document.getElementsByTagName('li');
+  var liInRankedList =
+
+  var ranked = document.getElementsByClassName('ranked-list');
+  //get all lis in ranked-list
+  //increase li by n
+
+
+  // for (i = 0; i > ranked.length; i++) {
+  //   var num = parseInt(ranked[i].querySelectorAll('li')[i].innerText)
+  //   var increaseNum = num + 1;
+  //   ranked[i].querySelectorAll('li')[i].innerText = increaseNum
+  // }
+}
+
+function deepestChild(){
+
+}
